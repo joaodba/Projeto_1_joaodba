9728089429a0658f648f67789c48b345201fa255
João Junior <jjrsuporteti@gmail.com>
2022-05-17 19:06:58 -0300

    Programação Python - Classificar listas

--- programacao Python.sql
+++ programacao Python.sql
@@ -1104,3 +1104,579 @@ Operator  Name                   Description
 >>        Signed right shift     Shift right by pushing copies of the leftmost bit in from the left, and let the rightmost bits fall off
 
 
+Lista
+
+As listas são usadas para armazenar vários itens em uma única variável.
+
+As listas são um dos 4 tipos de dados internos do Python usados para armazenar coleções de dados, os outros 3 são Tuple , Set e Dictionary , todos com qualidades e usos diferentes.
+
+As listas são criadas usando colchetes:
+
+Exemplo
+Crie uma lista:
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+
+lista de itens
+Os itens da lista são ordenados, alteráveis e permitem valores duplicados.
+
+Os itens da lista são indexados, o primeiro item possui índice [0], o segundo item possui índice [1]etc.
+
+Encomendado
+
+Quando dizemos que as listas estão ordenadas, significa que os itens têm uma ordem definida, e essa ordem não será alterada.
+
+Se você adicionar novos itens a uma lista, os novos itens serão colocados no final da lista.
+
+Nota: Existem alguns métodos de lista que irão alterar a ordem, mas em geral: a ordem dos itens não será alterada.
+
+Mutável
+
+A lista é mutável, o que significa que podemos alterar, adicionar e remover itens em uma lista após ela ter sido criada.
+
+Permitir duplicatas
+
+Como as listas são indexadas, as listas podem ter itens com o mesmo valor:
+
+Exemplo
+
+As listas permitem valores duplicados:
+
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+print(thislist)
+
+
+Comprimento da lista
+
+Para determinar quantos itens uma lista possui, use a len()função:
+
+Exemplo
+Imprima o número de itens na lista:
+
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+
+Listar itens - Tipos de dados
+Os itens da lista podem ser de qualquer tipo de dados:
+
+Exemplo
+
+Tipos de dados String, int e boolean:
+
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+
+Uma lista pode conter diferentes tipos de dados:
+
+Exemplo
+Uma lista com strings, números inteiros e valores booleanos:
+
+list1 = ["abc", 34, True, 40, "male"]
+tipo()
+
+Da perspectiva do Python, as listas são definidas como objetos com o tipo de dados 'list':
+
+<class 'list'>
+Exemplo
+Qual é o tipo de dados de uma lista?
+
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist))
+
+O construtor list()
+Também é possível usar o construtor list() ao criar uma nova lista.
+
+Exemplo
+Usando o list()construtor para fazer uma lista:
+
+thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
+print(thislist)
+
+Coleções Python (matrizes)
+Existem quatro tipos de dados de coleção na linguagem de programação Python:
+
+Lista      é uma coleção que é ordenada e mutável. Permite membros duplicados.
+Tupla      é uma coleção ordenada e imutável. Permite membros duplicados.
+Set        é uma coleção não ordenada, imutável* e não indexada. Nenhum membro duplicado.
+Dicionário é uma coleção ordenada** e mutável. Nenhum membro duplicado.
+
+*Os itens do conjunto são imutáveis, mas você pode remover e/ou adicionar itens sempre que quiser.
+
+**A partir da versão 3.7 do Python, os dicionários são ordenados . No Python 3.6 e anteriores, os dicionários não são ordenados .
+
+Ao escolher um tipo de coleção, é útil entender as propriedades desse tipo. Escolher o tipo certo para um determinado conjunto de dados pode significar retenção de significado e pode significar um aumento na eficiência ou segurança.
+
+
+Acessar itens
+
+Os itens da lista são indexados e você pode acessá-los consultando o número do índice:
+
+Exemplo
+Imprima o segundo item da lista:
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist[1])
+
+Nota: O primeiro item tem índice 0.
+
+Indexação negativa
+
+Indexação negativa significa começar do fim
+
+-1 refere-se ao último item, -2 refere-se ao penúltimo item etc.
+
+Exemplo
+Imprima o último item da lista:
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist[-1])
+
+Faixa de índices
+
+Você pode especificar um intervalo de índices especificando onde começar e onde terminar o intervalo.
+
+Ao especificar um intervalo, o valor de retorno será uma nova lista com os itens especificados.
+
+Exemplo
+Retorne o terceiro, quarto e quinto item:
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])
+
+Nota: A pesquisa começará no índice 2 (incluído) e terminará no índice 5 (não incluído).
+
+Lembre-se que o primeiro item tem índice 0.
+
+Ao deixar de fora o valor inicial, o intervalo começará no primeiro item:
+
+Exemplo
+Este exemplo retorna os itens do início, mas NÃO incluindo, "kiwi":
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[:4])
+
+Ao deixar de fora o valor final, o intervalo irá para o final da lista:
+
+Exemplo
+Este exemplo retorna os itens de "cherry" até o final:
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:])
+
+Faixa de índices negativos
+
+Especifique índices negativos se desejar iniciar a pesquisa a partir do final da lista:
+
+Exemplo
+Este exemplo retorna os itens de "laranja" (-4) para, mas NÃO incluindo "manga" (-1):
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[-4:-1])
+
+Verifique se o item existe
+
+Para determinar se um item especificado está presente em uma lista, use a inpalavra-chave:
+
+Exemplo
+Verifique se "apple" está presente na lista:
+
+thislist = ["apple", "banana", "cherry"]
+if "apple" in thislist:
+  print("Yes, 'apple' is in the fruits list")
+
+
+Alterar valor do item
+
+Para alterar o valor de um item específico, consulte o número do índice:
+
+Exemplo
+Altere o segundo item:
+
+thislist = ["apple", "banana", "cherry"]
+thislist[1] = "blackcurrant"
+print(thislist)
+
+Alterar um intervalo de valores de itens
+
+Para alterar o valor dos itens dentro de um intervalo específico, defina uma lista com os novos valores e consulte o intervalo de números de índice onde deseja inserir os novos valores:
+
+Exemplo
+Altere os valores "banana" e "cherry" pelos valores "blackcurrant" e "watermelon":
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+thislist[1:3] = ["blackcurrant", "watermelon"]
+print(thislist)
+
+Se você inserir mais itens do que substituir, os novos itens serão inseridos onde você especificou e os itens restantes serão movidos de acordo:
+
+Exemplo
+
+Altere o segundo valor substituindo-o por dois novos valores:
+
+thislist = ["apple", "banana", "cherry"]
+thislist[1:2] = ["blackcurrant", "watermelon"]
+print(thislist)
+
+Nota: O comprimento da lista mudará quando o número de itens inseridos não corresponder ao número de itens substituídos.
+
+Se você inserir menos itens do que substituir, os novos itens serão inseridos onde você especificou e os itens restantes serão movidos de acordo:
+
+Exemplo
+Altere o segundo e o terceiro valor substituindo-o por um valor:
+
+thislist = ["apple", "banana", "cherry"]
+thislist[1:3] = ["watermelon"]
+print(thislist)
+
+
+Inserir itens
+Para inserir um novo item de lista, sem substituir nenhum dos valores existentes, podemos usar o insert()método.
+
+O insert()método insere um item no índice especificado:
+
+Exemplo
+Insira "melancia" como terceiro item:
+
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(2, "watermelon")
+print(thislist)
+
+
+Anexar itens
+
+Para adicionar um item ao final da lista, use o método append() :
+
+Exemplo
+Usando o append()método para anexar um item:
+
+thislist = ["apple", "banana", "cherry"]
+thislist.append("orange")
+print(thislist)
+
+Inserir itens
+Para inserir um item de lista em um índice especificado, use o insert()método.
+
+O insert()método insere um item no índice especificado:
+
+Exemplo
+Insira um item como a segunda posição:
+
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(1, "orange")
+print(thislist)
+
+Nota: Como resultado dos exemplos acima, as listas agora conterão 4 itens.
+
+Estender lista
+Para anexar elementos de outra lista à lista atual, use o extend()método.
+
+Exemplo
+Adicione os elementos de tropicala thislist:
+
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+thislist.extend(tropical)
+print(thislist)
+
+Os elementos serão adicionados ao final da lista.
+
+Adicionar qualquer iterável
+O extend()método não precisa anexar listas , você pode adicionar qualquer objeto iterável (tuplas, conjuntos, dicionários etc.).
+
+Exemplo
+Adicionar elementos de uma tupla a uma lista:
+
+thislist = ["apple", "banana", "cherry"]
+thistuple = ("kiwi", "orange")
+thislist.extend(thistuple)
+print(thislist)
+
+Remover item especificado
+O remove()método remove o item especificado.
+
+Exemplo
+Remova a "banana":
+
+thislist = ["apple", "banana", "cherry"]
+thislist.remove("banana")
+print(thislist)
+
+Remover índice especificado
+O pop()método remove o índice especificado.
+
+Exemplo
+Remova o segundo item:
+
+thislist = ["apple", "banana", "cherry"]
+thislist.pop(1)
+print(thislist)
+
+Se você não especificar o índice, o pop()método removerá o último item.
+
+Exemplo
+Remova o último item:
+
+thislist = ["apple", "banana", "cherry"]
+thislist.pop()
+print(thislist)
+
+A palavra- delchave também remove o índice especificado:
+
+Exemplo
+Remova o primeiro item:
+
+thislist = ["apple", "banana", "cherry"]
+del thislist[0]
+print(thislist)
+
+A delpalavra-chave também pode excluir a lista completamente.
+
+Exemplo
+Apague a lista inteira:
+
+thislist = ["apple", "banana", "cherry"]
+del thislist
+
+Limpar a lista
+O clear()método esvazia a lista.
+
+A lista ainda permanece, mas não tem conteúdo.
+
+Exemplo
+Limpe o conteúdo da lista:
+
+thislist = ["apple", "banana", "cherry"]
+thislist.clear()
+print(thislist)
+
+Percorrer uma lista
+
+Você pode percorrer os itens da lista usando um for loop:
+
+Exemplo
+Imprima todos os itens da lista, um por um:
+
+thislist = ["apple", "banana", "cherry"]
+for x in thislist:
+  print(x)
+
+Percorrer os números de índice
+Você também pode percorrer os itens da lista consultando seu número de índice.
+
+Use as funções range()e len()para criar um iterável adequado.
+
+Exemplo
+Imprima todos os itens consultando seu número de índice:
+
+thislist = ["apple", "banana", "cherry"]
+for i in range(len(thislist)):
+  print(thislist[i])
+
+O iterável criado no exemplo acima é [0, 1, 2].
+
+Usando um loop while
+Você pode percorrer os itens da lista usando um whileloop.
+
+Use a len()função para determinar o comprimento da lista, então comece em 0 e faça um loop pelos itens da lista consultando seus índices.
+
+Lembre-se de aumentar o índice em 1 após cada iteração.
+
+Exemplo
+Imprima todos os itens, usando um whileloop para percorrer todos os números de índice
+
+thislist = ["apple", "banana", "cherry"]
+i = 0
+while i < len(thislist):
+  print(thislist[i])
+  i = i + 1
+
+Loop usando compreensão de lista
+O List Compreension oferece a sintaxe mais curta para percorrer listas:
+
+Exemplo
+Um forloop de mão curto que imprimirá todos os itens em uma lista:
+
+thislist = ["apple", "banana", "cherry"]
+[print(x) for x in thislist]
+
+
+Compreensão da lista
+A compreensão de lista oferece uma sintaxe mais curta quando você deseja criar uma nova lista com base nos valores de uma lista existente.
+
+Exemplo:
+
+Com base em uma lista de frutas, você deseja uma nova lista, contendo apenas as frutas com a letra "a" no nome.
+
+Sem compreensão de lista, você terá que escrever uma fordeclaração com um teste condicional dentro:
+
+Exemplo
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = []
+
+for x in fruits:
+  if "a" in x:
+    newlist.append(x)
+
+print(newlist)
+
+Com compreensão de lista, você pode fazer tudo isso com apenas uma linha de código:
+
+Exemplo
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+
+newlist = [x for x in fruits if "a" in x]
+
+print(newlist)
+
+
+A Sintaxe
+newlist = [expression for item in iterable if condition == True]
+O valor de retorno é uma nova lista, deixando a lista antiga inalterada.
+
+Doença
+A condição é como um filtro que aceita apenas os itens que avaliam como True.
+
+Exemplo
+Aceite apenas itens que não sejam "maçã":
+
+newlist = [x for x in fruits if x != "apple"]
+A condição if x != "apple"  retornará True para todos os elementos exceto "maçã", fazendo com que a nova lista contenha todas as frutas, exceto "maçã".
+
+A condição é opcional e pode ser omitida:
+
+Exemplo
+Sem if declaração:
+
+newlist = [x for x in fruits]
+
+Iterável
+O iterável pode ser qualquer objeto iterável, como uma lista, tupla, conjunto etc.
+
+Exemplo
+Você pode usar a range()função para criar um iterável:
+
+newlist = [x for x in range(10)]
+Mesmo exemplo, mas com uma condição:
+
+Exemplo
+Aceite apenas números inferiores a 5:
+
+newlist = [x for x in range(10) if x < 5]
+
+Expressão
+A expressão é o item atual na iteração, mas também é o resultado, que você pode manipular antes que termine como um item de lista na nova lista:
+
+Exemplo
+Defina os valores na nova lista para maiúsculas:
+
+newlist = [x.upper() for x in fruits]
+
+Você pode definir o resultado para o que quiser:
+
+Exemplo
+Defina todos os valores na nova lista para 'hello':
+
+newlist = ['hello' for x in fruits]
+
+A expressão também pode conter condições, não como um filtro, mas como forma de manipular o resultado:
+
+Exemplo
+Retorne "laranja" em vez de "banana":
+
+newlist = [x if x != "banana" else "orange" for x in fruits]
+
+
+Classificar lista alfanumérica
+
+Objetos de lista possuem um sort()método que ordenará a lista de forma alfanumérica, em ordem crescente, por padrão:
+
+Exemplo
+Classifique a lista em ordem alfabética:
+
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort()
+print(thislist)
+
+Exemplo
+Classifique a lista numericamente:
+
+thislist = [100, 50, 65, 82, 23]
+thislist.sort()
+print(thislist)
+
+Classificação decrescente
+Para classificar de forma decrescente, use o argumento de palavra-chave reverse = True:
+
+Exemplo
+Ordene a lista decrescente:
+
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort(reverse = True)
+print(thislist)
+
+Exemplo
+Ordene a lista decrescente:
+
+thislist = [100, 50, 65, 82, 23]
+thislist.sort(reverse = True)
+print(thislist)
+
+Personalizar a função de classificação
+
+Você também pode personalizar sua própria função usando o argumento de palavra-chave .key = function
+
+A função retornará um número que será usado para ordenar a lista (o número mais baixo primeiro):
+
+Exemplo
+Classifique a lista com base em quão próximo o número está de 50:
+
+def myfunc(n):
+  return abs(n - 50)
+
+thislist = [100, 50, 65, 82, 23]
+thislist.sort(key = myfunc)
+print(thislist)
+
+Classificação sem distinção entre maiúsculas e minúsculas
+
+Por padrão, o sort()método diferencia maiúsculas de minúsculas, resultando em todas as letras maiúsculas classificadas antes das minúsculas:
+
+Exemplo
+
+A classificação com distinção entre maiúsculas e minúsculas pode fornecer um resultado inesperado:
+
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.sort()
+print(thislist)
+
+Felizmente, podemos usar funções internas como funções-chave ao classificar uma lista.
+
+Portanto, se você deseja uma função de classificação que não diferencia maiúsculas de minúsculas, use str.lower como uma função de chave:
+
+Exemplo
+
+Execute uma classificação que não diferencia maiúsculas de minúsculas da lista:
+
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.sort(key = str.lower)
+print(thislist)
+
+Ordem reversa
+
+E se você quiser inverter a ordem de uma lista, independentemente do alfabeto?
+
+O reverse()método inverte a ordem de classificação atual dos elementos.
+
+Exemplo
+Inverta a ordem dos itens da lista:
+
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.reverse()
+print(thislist)
+
+
+
+
+
