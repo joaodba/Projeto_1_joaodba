fcfe8c4c3b3242d9fa391ad066842c32656a41fd
João Junior <jjrsuporteti@gmail.com>
2022-05-30 18:24:43 -0300

    Programação Python - Funções do Python

--- programacao Python.sql
+++ programacao Python.sql
@@ -3070,6 +3070,7 @@ if b > a:
   print("b is greater than a")
 elif a == b:
   print("a and b are equal")
+
 Neste exemplo a é igual a b , então a primeira condição não é verdadeira, mas a condição elif 
 é verdadeira, então imprimimos na tela que "a e b são iguais".
 
@@ -3100,6 +3101,7 @@ else:
   print("b is not greater than a")
 
 Mão curta se
+
 Se você tiver apenas uma instrução para executar, poderá colocá-la na mesma linha da instrução if.
 
 Exemplo
@@ -3143,7 +3145,7 @@ if a > b and c > a:
   print("Both conditions are True")
 
 Ou
-A orpalavra-chave é um operador lógico e é usada para combinar instruções condicionais:
+A or palavra-chave é um operador lógico e é usada para combinar instruções condicionais:
 
 Exemplo
 Teste se a for maior que b, OU se a for maior que c:
@@ -3178,4 +3180,406 @@ b = 200
 if b > a:
   pass
   
+
+Loops Python
+
+Python tem dois comandos de loop primitivos:
+
+loops while
+para laços
+
+O loop while
+Com o loop while podemos executar um conjunto de instruções desde que uma condição seja verdadeira.
+
+Exemplo
+Imprima i enquanto i for menor que 6:
+
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+
+Nota: lembre-se de incrementar i, senão o loop continuará para sempre.
+
+O loop while requer que as variáveis relevantes estejam prontas, neste exemplo precisamos definir
+ uma variável de indexação, i , que definimos como 1.
+
+A declaração de pausa
+
+Com a instrução break podemos parar o loop mesmo se a condição while for verdadeira:
+
+Exemplo
+Saia do loop quando i for 3:
+
+i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+  i += 1
+
+A declaração continua
+Com a instrução continue podemos parar a iteração atual e continuar com a próxima:
+
+Exemplo
+
+Continue para a próxima iteração se i for 3:
+
+i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i)
+
+A declaração else
+
+Com a instrução else podemos executar um bloco de código uma vez quando a condição não for mais
+verdadeira:
+
+Exemplo
+Imprima uma mensagem quando a condição for falsa:
+
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6")
+
+
+Python para loops
+
+Um loop for é usado para iterar sobre uma sequência (que é uma lista, uma tupla, um dicionário,
+ um conjunto ou uma string).
+
+Isso é menos parecido com a palavra-chave for em outras linguagens de programação e funciona mais 
+como um método iterador, conforme encontrado em outras linguagens de programação orientadas a objetos.
+
+Com o loop for podemos executar um conjunto de instruções, uma vez para cada item de uma lista, tupla,
+ conjunto etc.
+
+Exemplo
+Imprima cada fruta em uma lista de frutas:
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+O loop for não requer que uma variável de indexação seja definida antecipadamente.
+
+Fazendo um loop em uma string
+Mesmo as strings são objetos iteráveis, elas contêm uma sequência de caracteres:
+
+Exemplo
+Percorra as letras da palavra "banana":
+
+for x in "banana":
+  print(x)
+
+A declaração de pausa
+Com a instrução break podemos parar o loop antes que ele tenha percorrido todos os itens:
+
+Exemplo
+Saia do loop quando xfor "banana":
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+  if x == "banana":
+    break
+
+Exemplo
+
+Saia do loop quando x for "banana", mas dessa vez o break vem antes do print:
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+  print(x)
+
+A declaração continua
+Com a instrução continue podemos parar a iteração atual do loop e continuar com a próxima:
+
+Exemplo
+Não imprima banana:
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    continue
+  print(x)
+
+A função intervalo()
+
+Para percorrer um conjunto de código um determinado número de vezes, podemos usar a função range() ,
+A função range() retorna uma sequência de números, começando em 0 por padrão e incrementa em 1 
+(por padrão) e termina em um número especificado.
+
+Exemplo
+Usando a função range():
+
+for x in range(6):
+  print(x)
+
+Observe que range(6) não são os valores de 0 a 6, mas os valores de 0 a 5.
+
+A função range() tem como padrão 0 como valor inicial, porém é possível especificar o valor inicial 
+adicionando um parâmetro: range(2, 6) , que significa valores de 2 a 6 (mas não incluindo 6):
+
+Exemplo
+Usando o parâmetro inicial:
+
+for x in range(2, 6):
+  print(x)
+
+A função range() padroniza para incrementar a sequência em 1, porém é possível especificar o valor do
+ incremento adicionando um terceiro parâmetro: range(2, 30, 3 ) :
+
+Exemplo
+Incremente a sequência com 3 (o padrão é 1):
+
+for x in range(2, 30, 3):
+  print(x)
+
+Mais em For Loop
+A elsepalavra-chave em um forloop especifica um bloco de código a ser executado quando o loop for concluído:
+
+Exemplo
+Imprima todos os números de 0 a 5 e imprima uma mensagem quando o loop terminar:
+
+for x in range(6):
+  print(x)
+else:
+  print("Finally finished!")
+
+Nota: O elsebloco NÃO será executado se o loop for interrompido por uma breakinstrução.
+
+Exemplo
+Quebre o loop quando xfor 3 e veja o que acontece com o elsebloco:
+
+for x in range(6):
+  if x == 3: break
+  print(x)
+else:
+  print("Finally finished!")
+
+Loops aninhados
+Um loop aninhado é um loop dentro de um loop.
+
+O "loop interno" será executado uma vez para cada iteração do "loop externo":
+
+Exemplo
+Imprima cada adjetivo para cada fruta:
+
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+  for y in fruits:
+    print(x, y)
+
+A declaração de passagem
+forloops não podem estar vazios, mas se por algum motivo você tiver um forloop sem conteúdo, 
+coloque a passinstrução para evitar erros.
+
+Exemplo
+for x in [0, 1, 2]:
+  pass
+
+  
+Funções do Python
+Uma função é um bloco de código que só é executado quando é chamado.
+
+Você pode passar dados, conhecidos como parâmetros, para uma função.
+
+Uma função pode retornar dados como resultado.
+
+Criando uma função
+Em Python, uma função é definida usando a palavra-chave def :
+
+Exemplo
+def my_function():
+  print("Hello from a function")
+Chamando uma função
+Para chamar uma função, use o nome da função seguido de parênteses:
+
+Exemplo
+def my_function():
+  print("Hello from a function")
+
+my_function()
+Argumentos
+As informações podem ser passadas para funções como argumentos.
+
+Os argumentos são especificados após o nome da função, dentro dos parênteses. Você pode adicionar 
+quantos argumentos quiser, basta separá-los com uma vírgula.
+
+O exemplo a seguir tem uma função com um argumento (fname). Quando a função é chamada, passamos um 
+primeiro nome, que é usado dentro da função para imprimir o nome completo:
+
+Exemplo
+def my_function(fname):
+  print(fname + " Refsnes")
+
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
+Os argumentos geralmente são abreviados para args nas documentações do Python.
+
+
+Parâmetros ou argumentos?
+Os termos parâmetro e argumento podem ser usados para a mesma coisa: informações que são passadas 
+para uma função.
+
+Do ponto de vista de uma função:
+
+Um parâmetro é a variável listada entre parênteses na definição da função.
+
+Um argumento é o valor que é enviado para a função quando ela é chamada.
+
+Número de argumentos
+Por padrão, uma função deve ser chamada com o número correto de argumentos. Isso significa que, se sua 
+função espera 2 argumentos, você deve chamar a função com 2 argumentos, nem mais, nem menos.
+
+Exemplo
+Esta função espera 2 argumentos e obtém 2 argumentos:
+
+def my_function(fname, lname):
+  print(fname + " " + lname)
+
+my_function("Emil", "Refsnes")
+Se você tentar chamar a função com 1 ou 3 argumentos, receberá um erro:
+Exemplo
+Esta função espera 2 argumentos, mas obtém apenas 1:
+
+def my_function(fname, lname):
+  print(fname + " " + lname)
+
+my_function("Emil")
+Argumentos arbitrários, *args
+Se você não souber quantos argumentos serão passados para sua função, adicione um *antes do nome do 
+parâmetro na definição da função.
+
+Dessa forma, a função receberá uma tupla de argumentos e poderá acessar os itens de acordo:
+
+Exemplo
+Se o número de argumentos for desconhecido, adicione um *antes do nome do parâmetro:
+
+def my_function(*kids):
+  print("The youngest child is " + kids[2])
+
+my_function("Emil", "Tobias", "Linus")
+Argumentos arbitrários são frequentemente abreviados para *args nas documentações do Python.
+
+Argumentos de palavra-chave
+Você também pode enviar argumentos com a sintaxe key = value .
+
+Desta forma, a ordem dos argumentos não importa.
+
+Exemplo
+def my_function(child3, child2, child1):
+  print("The youngest child is " + child3)
+
+my_function(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
+A frase Argumentos de palavras -chave geralmente é abreviada para kwargs nas documentações do Python.
+
+Argumentos arbitrários de palavras-chave, **kwargs
+Se você não souber quantos argumentos de palavra-chave serão passados para sua função, adicione dois 
+asteriscos: **antes do nome do parâmetro na definição da função.
+
+Desta forma a função receberá um dicionário de argumentos, podendo acessar os itens de acordo:
+
+Exemplo
+Se o número de argumentos de palavra-chave for desconhecido, adicione um duplo **antes do nome do parâmetro:
+
+def my_function(**kid):
+  print("His last name is " + kid["lname"])
+
+my_function(fname = "Tobias", lname = "Refsnes")
+Argumentos Kword arbitrários são frequentemente abreviados para **kwargs nas documentações do Python.
+
+Valor do parâmetro padrão
+O exemplo a seguir mostra como usar um valor de parâmetro padrão.
+
+Se chamarmos a função sem argumento, ela usará o valor padrão:
+
+Exemplo
+def my_function(country = "Norway"):
+  print("I am from " + country)
+
+my_function("Sweden")
+my_function("India")
+my_function()
+my_function("Brazil")
+Passando uma lista como um argumento
+Você pode enviar qualquer tipo de dado de argumento para uma função (string, número, lista, dicionário etc.), 
+e ele será tratado como o mesmo tipo de dado dentro da função.
+
+Por exemplo, se você enviar uma lista como argumento, ela ainda será uma lista quando chegar à função:
+
+Exemplo
+def my_function(food):
+  for x in food:
+    print(x)
+
+fruits = ["apple", "banana", "cherry"]
+
+my_function(fruits)
+Valores de retorno
+Para permitir que uma função retorne um valor, use a return instrução:
+
+Exemplo
+def my_function(x):
+  return 5 * x
+
+print(my_function(3))
+print(my_function(5))
+print(my_function(9))
+
+A declaração de passagem
+functionAs definições não podem estar vazias, mas se por algum motivo você tiver uma functiondefinição sem
+ conteúdo, coloque a passdeclaração para evitar erros.
+
+Exemplo
+
+def myfunction():
+  pass
+
+Recursão
+
+Python também aceita recursão de função, o que significa que uma função definida pode chamar a si mesma.
+
+A recursão é um conceito matemático e de programação comum. Isso significa que uma função chama a si mesma.
+ Isso tem a vantagem de significar que você pode percorrer os dados para chegar a um resultado.
+
+O desenvolvedor deve ter muito cuidado com a recursão, pois pode ser muito fácil escrever uma função que nunca 
+termina, ou uma que usa quantidades excessivas de memória ou poder do processador. No entanto, quando escrita 
+corretamente, a recursão pode ser uma abordagem de programação muito eficiente e matematicamente elegante.
+
+Neste exemplo, tri_recursion() é uma função que definimos para chamar a si mesma ("recurse"). Usamos a variável k
+ como os dados, que decrementam ( -1 ) toda vez que recorremos. A recursão termina quando a condição não for maior 
+ que 0 (ou seja, quando for 0).
+
+Para um novo desenvolvedor, pode levar algum tempo para descobrir exatamente como isso funciona, a melhor maneira de 
+descobrir é testando e modificando.
+
+Exemplo
+Exemplo de recursão
+
+def tri_recursion(k):
+  if(k > 0):
+    result = k + tri_recursion(k - 1)
+    print(result)
+  else:
+    result = 0
+  return result
+
+print("\n\nRecursion Example Results")
+tri_recursion(6)
+
+
+
+
 
